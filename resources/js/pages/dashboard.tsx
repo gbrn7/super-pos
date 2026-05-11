21c8935bfbd7449e6c4d59eedba2aff67e8c0e1c
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2026-05-11 21:10:38 +0700

    update

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,14 +1,26 @@
 import { Head } from '@inertiajs/react';
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { dashboard } from '@/routes';
 import i18next from 'i18next';
+import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
+import { Badge } from "@/components/ui/badge"
+import {
+    Card,
+    CardAction,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { ChartAreaInteractive } from './chard-area-interactive';
+import { DataTable } from './data-table';
+import data from "./data.json"
 
 export default function Dashboard() {
     return (
         <>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
@@ -21,6 +33,101 @@ export default function Dashboard() {
                 </div>
                 <div className="relative min-h-screen flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                     <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                </div> */}
+
+                <div className="@container/main flex flex-1 flex-col gap-2">
+                    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                        <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
+                            <Card className="@container/card">
+                                <CardHeader>
+                                    <CardDescription>Total Revenue</CardDescription>
+                                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                                        $1,250.00
+                                    </CardTitle>
+                                    <CardAction>
+                                        <Badge variant="outline">
+                                            <IconTrendingUp />
+                                            +12.5%
+                                        </Badge>
+                                    </CardAction>
+                                </CardHeader>
+                                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                                    <div className="line-clamp-1 flex gap-2 font-medium">
+                                        Trending up this month <IconTrendingUp className="size-4" />
+                                    </div>
+                                    <div className="text-muted-foreground">
+                                        Visitors for the last 6 months
+                                    </div>
+                                </CardFooter>
+                            </Card>
+                            <Card className="@container/card">
+                                <CardHeader>
+                                    <CardDescription>New Customers</CardDescription>
+                                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                                        1,234
+                                    </CardTitle>
+                                    <CardAction>
+                                        <Badge variant="outline">
+                                            <IconTrendingDown />
+                                            -20%
+                                        </Badge>
+                                    </CardAction>
+                                </CardHeader>
+                                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                                    <div className="line-clamp-1 flex gap-2 font-medium">
+                                        Down 20% this period <IconTrendingDown className="size-4" />
+                                    </div>
+                                    <div className="text-muted-foreground">
+                                        Acquisition needs attention
+                                    </div>
+                                </CardFooter>
+                            </Card>
+                            <Card className="@container/card">
+                                <CardHeader>
+                                    <CardDescription>Active Accounts</CardDescription>
+                                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                                        45,678
+                                    </CardTitle>
+                                    <CardAction>
+                                        <Badge variant="outline">
+                                            <IconTrendingUp />
+                                            +12.5%
+                                        </Badge>
+                                    </CardAction>
+                                </CardHeader>
+                                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                                    <div className="line-clamp-1 flex gap-2 font-medium">
+                                        Strong user retention <IconTrendingUp className="size-4" />
+                                    </div>
+                                    <div className="text-muted-foreground">Engagement exceed targets</div>
+                                </CardFooter>
+                            </Card>
+                            <Card className="@container/card">
+                                <CardHeader>
+                                    <CardDescription>Growth Rate</CardDescription>
+                                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                                        4.5%
+                                    </CardTitle>
+                                    <CardAction>
+                                        <Badge variant="outline">
+                                            <IconTrendingUp />
+                                            +4.5%
+                                        </Badge>
+                                    </CardAction>
+                                </CardHeader>
+                                <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                                    <div className="line-clamp-1 flex gap-2 font-medium">
+                                        Steady performance increase <IconTrendingUp className="size-4" />
+                                    </div>
+                                    <div className="text-muted-foreground">Meets growth projections</div>
+                                </CardFooter>
+                            </Card>
+                        </div>
+                        <div className="px-4 lg:px-6">
+                            <ChartAreaInteractive />
+                        </div>
+                        <DataTable data={data} />
+                    </div>
                 </div>
             </div>
         </>
