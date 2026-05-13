6375b317b7ccfb678dc9f365be28efd4fad80541
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2026-05-13 23:20:34 +0700

    feat: add laravel telescope

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,6 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'superuser@gmail.com',
+        ]);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
