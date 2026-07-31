c77674cc89b02e750532d27f824101db2bad7a83
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2026-08-01 00:55:03 +0700

    redirect root url to dashboard

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,10 @@ use App\Http\Controllers\TransactionDetailController;
 use App\Http\Controllers\UnitController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
