50a888fb739c339cf098384c2a81db038c339764
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2026-08-02 13:26:59 +0700

    update permission seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -7,13 +7,17 @@ use App\Models\Role;
 use App\Support\Enums\CapitalWalletPermissionEnums;
 use App\Support\Enums\CategoryPermissionEnums;
 use App\Support\Enums\DashboardPermissionEnums;
+use App\Support\Enums\MasterProductPermissionEnums;
 use App\Support\Enums\PaymentMethodPermissionEnums;
+use App\Support\Enums\ProductPermissionEnums;
 use App\Support\Enums\ProfitWalletPermissionEnums;
 use App\Support\Enums\ReturnPermissionEnums;
 use App\Support\Enums\RoleEnums;
 use App\Support\Enums\RolePermissionEnums;
+use App\Support\Enums\TransactionDetailPermissionEnums;
 use App\Support\Enums\TransactionPermissionEnums;
 use App\Support\Enums\UnitPermissionEnums;
+use App\Support\Enums\UserPermissionEnums;
 use Illuminate\Database\Seeder;
 
 class PermissionSeeder extends Seeder
@@ -23,6 +27,30 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
+        foreach (MasterProductPermissionEnums::cases() as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission->value,
+            ]);
+        }
+
+        foreach (UserPermissionEnums::cases() as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission->value,
+            ]);
+        }
+
+        foreach (TransactionDetailPermissionEnums::cases() as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission->value,
+            ]);
+        }
+
+        foreach (ProductPermissionEnums::cases() as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission->value,
+            ]);
+        }
+
         foreach (CategoryPermissionEnums::cases() as $permission) {
             Permission::firstOrCreate([
                 'name' => $permission->value,
