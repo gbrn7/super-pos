f533fe13d374f58cf0e5acc343ce9d7901008d9f
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2026-08-09 19:47:42 +0700

    fix(i18n): dynamic X-Language header in axios and sync localStorage language state

--- resources/js/lib/axios.ts
+++ resources/js/lib/axios.ts
@@ -1,23 +1,27 @@
-import { LanguageSystem } from '@/constants/Index';
+import { languageCode, LanguageSystem, localStorageKey } from '@/constants/Index';
 import axios from 'axios';
+import constants from 'constants';
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        'X-Language': LanguageSystem,
     },
 });
 
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
+    const lang = LanguageSystem
 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
 
+    config.headers['X-Language'] = lang;
+
     return config;
 });
 
 export default axiosInstance;
+
